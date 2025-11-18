f1a7e036980d9c997d31dc76e5bb647a2ea3694e
Craig Johnson <cjohns65@terpmail.umd.edu>
2025-11-18 17:54:28 -0500

    feat(server): production NODE_ENV

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 console.log('→ connecting to', process.env.MONGO_URI);
 
 const express           = require('express');
